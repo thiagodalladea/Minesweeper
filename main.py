9331dca79d351ac8c92debec07f8ed2249767541
Thiago Dalla Dea <thiagopdalladea@gmail.com>
2022-05-13 05:28:29 -0300

    Terminando a formação da matriz
    Verificação se há minas ou não em todas as casas da matriz;
    Usuário escolhe o tamanho da matriz (9, 16, 30);
    
    Resumindo: base do jogo pronta

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import numpy as np
 import pygame as pg
 import random
 
-size = 9
+size = int(input())
 count = 0
 
 def defineBomb(x, y):
   if(field[x][y] == 0):
     field[x][y] = -1
   else:
-    defineBomb(random.randint(0,8), random.randint(0,8))
+    defineBomb(random.randint(0,size-1), random.randint(0,size-1))
 
-field = np.zeros((9,9), dtype=np.int8)
+field = np.zeros((size,size), dtype=np.int8)
 
-for i in range(10):
-  defineBomb(random.randint(0,8), random.randint(0,8))
+if(size == 9):
+  for i in range(10):
+    defineBomb(random.randint(0,size-1), random.randint(0,size-1))
+elif(size == 16):
+  for i in range(40):
+    defineBomb(random.randint(0,size-1), random.randint(0,size-1))
+elif(size == 30):
+  for i in range(150):
+    defineBomb(random.randint(0,size-1), random.randint(0,size-1))
 
 for i in range(size):
   for j in range(size):
     if(field[i][j] != -1):
-      if(i != 0 and j != 0 and i != 8 and j != 8):
+      if(i != 0 and j != 0 and i != size-1 and j != size-1):
         if(field[i-1][j-1] == -1):
           count += 1
         if(field[i][j-1] == -1):
@@ -36,6 +43,111 @@ for i in range(size):
           count += 1
         if(field[i+1][j+1] == -1):
           count += 1
+
+        field[i][j] = count
+        count = 0
+        
+      elif(i == 0 and j == 0):
+        if(field[i][j+1] == -1):
+          count += 1
+        if(field[i+1][j+1] == -1):
+          count += 1
+        if(field[i+1][j] == -1):
+          count += 1
+
+        field[i][j] = count
+        count = 0
+
+      elif(i == 0 and j == size-1):
+        if(field[i][j-1] == -1):
+          count += 1
+        if(field[i+1][j-1] == -1):
+          count += 1
+        if(field[i+1][j] == -1):
+          count += 1
+
+        field[i][j] = count
+        count = 0
+        
+      elif(i == size-1 and j == 0):
+        if(field[i-1][j] == -1):
+          count += 1
+        if(field[i-1][j+1] == -1):
+          count += 1
+        if(field[i][j+1] == -1):
+          count += 1
+
+        field[i][j] = count
+        count = 0
+        
+      elif(i == size-1 and j == size-1):
+        if(field[i-1][j] == -1):
+          count += 1
+        if(field[i-1][j-1] == -1):
+          count += 1
+        if(field[i][j-1] == -1):
+          count += 1
+
+        field[i][j] = count
+        count = 0
+        
+      elif(i == 0):
+        if(field[i][j-1] == -1):
+          count += 1
+        if(field[i+1][j-1] == -1):
+          count += 1
+        if(field[i+1][j] == -1):
+          count += 1
+        if(field[i+1][j+1] == -1):
+          count += 1
+        if(field[i][j+1] == -1):
+          count += 1
+
+        field[i][j] = count
+        count = 0
+
+      elif(i == size-1):
+        if(field[i][j-1] == -1):
+          count += 1
+        if(field[i-1][j-1] == -1):
+          count += 1
+        if(field[i-1][j] == -1):
+          count += 1
+        if(field[i-1][j+1] == -1):
+          count += 1
+        if(field[i][j+1] == -1):
+          count += 1
+
+        field[i][j] = count
+        count = 0
+
+      elif(j == 0):
+        if(field[i-1][j] == -1):
+          count += 1
+        if(field[i-1][j+1] == -1):
+          count += 1
+        if(field[i][j+1] == -1):
+          count += 1
+        if(field[i+1][j+1] == -1):
+          count += 1
+        if(field[i+1][j] == -1):
+          count += 1
+
+        field[i][j] = count
+        count = 0
+
+      elif(j == size-1):
+        if(field[i-1][j] == -1):
+          count += 1
+        if(field[i-1][j-1] == -1):
+          count += 1
+        if(field[i][j-1] == -1):
+          count += 1
+        if(field[i+1][j-1] == -1):
+          count += 1
+        if(field[i+1][j] == -1):
+          count += 1
+
         field[i][j] = count
         count = 0
 
