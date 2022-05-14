05c1d1e044d56ce4f910df0c3ac73eb1bf851f21
Thiago Dalla Dea <thiagopdalladea@gmail.com>
2022-05-14 11:20:16 -0300

    Tela inicial finalizada
    Todos os elementos visuais adicionados na tela inicial do campo minado.

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ window = pygame.display.set_mode((1920,1080))
 pygame.display.set_caption('Minesweeper')
 pygame.font.init()
 font_250 = pygame.font.Font('font/Anson-Regular.otf', 250)
+font_120 = pygame.font.Font('font/Anson-Regular.otf', 120)
 font_50 = pygame.font.Font('font/Anson-Regular.otf', 50)
 font_25 = pygame.font.Font('font/Anson-Regular.otf', 25)
 
 GRAY = (166,166,166)
-DARK_GREY = (20,20,20)
+DARK_GREY = (80,80,80)
 BLACK = (0,0,0)
 
 #---------- MAKING THE GAME FIELD ----------#
@@ -172,20 +173,40 @@ def start():
 
   flag_img = pygame.image.load('images/flag.png')
   flag_img = pygame.transform.scale(flag_img,(182,182))
+
   title_text = font_250.render('MineSweeper', True, BLACK)
   escape_text = font_25.render('Press ESC to quit', True, BLACK)
+  beg_text = font_50.render('Beginner', True, BLACK)
+  begSize_text = font_120.render('9x9', True, BLACK)
+  int_text = font_50.render('Intermediate', True, BLACK)
+  intSize_text = font_120.render('16x16', True, BLACK)
+  exp_text = font_50.render('Expert', True, BLACK)
+  expSize_text = font_120.render('30x30', True, BLACK)
 
   escape_rect = escape_text.get_rect(center=(1920 / 2, 1020))
   title_rect = title_text.get_rect(center=(1920 / 1.8,190))
+  beg_rect = beg_text.get_rect(center=(475,495))
+  begSize_rect = begSize_text.get_rect(center=(475,642))
+  int_rect = int_text.get_rect(center=(960,495))
+  intSize_rect = intSize_text.get_rect(center=(960,642))
+  exp_rect = exp_text.get_rect(center=(1445,495))
+  expSize_rect = expSize_text.get_rect(center=(1445,642))
+
   flag_rect = flag_img.get_rect(center=(1920 / 5.5, 200))
 
   window.fill(GRAY)
   window.blit(flag_img, flag_rect)
-  window.blit(title_text, title_rect)  
+  window.blit(title_text, title_rect)
+  window.blit(beg_text,beg_rect)
+  window.blit(begSize_text,begSize_rect)
+  window.blit(int_text,int_rect)
+  window.blit(intSize_text,intSize_rect)
+  window.blit(exp_text,exp_rect)
+  window.blit(expSize_text,expSize_rect)
   window.blit(escape_text,escape_rect)
 
-  pygame.draw.rect(window, BLACK, pygame.Rect(772,455,375,375), 3, 30)
   pygame.draw.rect(window, BLACK, pygame.Rect(287,455,375,375), 3, 30)
+  pygame.draw.rect(window, BLACK, pygame.Rect(772,455,375,375), 3, 30)
   pygame.draw.rect(window, BLACK, pygame.Rect(1257,455,375,375), 3, 30)
 
   pygame.display.flip()
